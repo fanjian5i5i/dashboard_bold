656b3ba0f4ff56316fb6f002727ff2f2a2c5022c
Fan <JianF@bra.boston.cob>
2019-10-17 09:14:56 -0400

    BOLD-3 | changed css on list selection

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -204,8 +204,7 @@ function Frame(props) {
           adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
           Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
           imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. 
         </Typography>
         <Typography paragraph>
           Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
