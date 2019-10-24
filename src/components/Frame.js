ca7093e879d464773939b67fa3a7092c08d18c27
FJ <fanjian5i5i@gmail.com>
2019-10-23 23:06:54 -0400

    added chart

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -22,7 +22,7 @@ import { createMuiTheme, makeStyles, ThemeProvider, useTheme } from '@material-u
 import { withAuth } from '@okta/okta-react';
 import Appbar from './Appbar';
 import logo from '../assets/img/logo.png';
-
+import PieChart from './charts/Pie';
 
 
 import { connect } from 'react-redux';
@@ -221,17 +221,7 @@ function Frame(props) {
       </nav>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt.
-        </Typography>
+        <PieChart/>
         <Typography paragraph>
           Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
           facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
